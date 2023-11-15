43d76b0a46269e757243ce7200cca09aa556634d
Mailen Lorena Alvarez <m.labarrere2@gmail.com>
2023-11-15 20:54:31 -0300

    Finaliza el carrusel

--- components/js/nosotros.js
+++ components/js/nosotros.js
@@ -58,15 +58,22 @@ hamburguer.addEventListener('click', handleHamburguer)
   });
 
 // Inicializa Swiper después de que el DOM esté listo
-document.addEventListener('DOMContentLoaded', function () {
-    var swiper = new Swiper('.swiper-container', {
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-    });
-  });
+// document.addEventListener('DOMContentLoaded', function () {
+//     var swiper = new Swiper('.swiper-container', {
+//       navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//       },
+//       pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//       },
+//     });
+//   });
+
+var swiper = new Swiper('.swiper-container', {
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+});
