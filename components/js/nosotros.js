55e0f61f1f86147ee5808e77c2b6334cdfd788e5
Mailen Lorena Alvarez <m.labarrere2@gmail.com>
2023-11-15 15:43:29 -0300

    Añade animaciones de scroll a pestaña nosotros

---
+++ components/js/nosotros.js
@@ -0,0 +1,32 @@
+
+  ScrollReveal().reveal('.reveal-on-scroll', {
+    duration: 1000,
+    delay: 200,
+    distance: '50px',
+    easing: 'ease-in-out',
+    origin: 'left',
+    reset: true,
+    viewFactor: 0.3, // Ajusta este valor para controlar cuánto tiene que hacer scroll el usuario antes de que la animación se active completamente
+  });
+
+  ScrollReveal().reveal('.reveal-on-scroll2', {
+    duration: 1000,
+    delay: 200,
+    distance: '50px',
+    easing: 'ease-in-out',
+    origin: 'bottom',
+    reset: true,
+    viewFactor: 0.5, // Ajusta este valor para controlar cuánto tiene que hacer scroll el usuario antes de que la animación se active completamente
+  });
+
+  
+  ScrollReveal().reveal('.reveal-on-scroll-text', {
+    duration: 1000,
+    delay: 200,
+    distance: '50px',
+    easing: 'ease-in-out',
+    origin: 'right',
+    reset: true,
+    viewFactor: 0.3, // Ajusta este valor para controlar cuánto tiene que hacer scroll el usuario antes de que la animación se active completamente
+  });
+ 
