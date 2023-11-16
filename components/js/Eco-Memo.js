d67d2a3bdeb59c412e524010370f93754f77801d
AgustinCrow <agustinyciel@gmail.com>
2023-11-16 08:25:28 -0300

    Elimine impresiones

--- components/js/Eco-Memo.js
+++ components/js/Eco-Memo.js
@@ -1,11 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const board = document.querySelector(".board")
-    const card_back = document.querySelector(".board .card:first-child .back")
-    const card_front = document.querySelector(".board .card:first-child .front")
     const all_back = document.querySelectorAll(".back")
     const all_front = document.querySelectorAll(".front")
-    console.log(all_front)
-
 
     for (let i = 0; i < all_back.length; i++) {
         all_back[i].addEventListener('click', () => {
